4f45bfb104d9b03cc9c1d16494945404e451bd1e
Kieran Mann <kmann@missionbaymedia.com>
2017-02-16 16:58:53 -0800

    still trying to get working on heroku

--- models/Work.js
+++ models/Work.js
@@ -14,7 +14,7 @@ var Work = new keystone.List('Work', {
 Work.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'Y', index: true },
+	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: {
